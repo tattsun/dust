0f0a1228fc259509546c0388732bb0af5858f1d5
tattsun <t.t.mc1192.sf@gmail.com>
2015-07-04 17:44:08 +0900

    Add: dust.h include all headers

---
+++ src/dust.h
@@ -0,0 +1,23 @@
+//
+// Created by tattsun on 15/07/04.
+//
+
+#ifndef DUST_DUST_H
+#define DUST_DUST_H
+
+#include "Tuple.h"
+#include "ByteChars.h"
+
+// async
+#include "async/Event.h"
+#include "async/AsyncTCPServer.h"
+
+// network
+#include "network/PacketBuffer.h"
+#include "network/TCPServer.h"
+
+// thread
+#include "thread/PThread.h"
+
+
+#endif //DUST_DUST_H
